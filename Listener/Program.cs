2209797ef9ab4a17129b86fa3492f8b8156f349e
Burim Hajrizaj <burim.hajrizaj@outlook.com>
2018-11-27 22:24:07 +0100

    first commit

---
+++ Listener/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listener
+{
+    class Program
+    {
+        private static string topic = "test";
+        private static string subscription = "listener";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----   This is a listener   -----");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            var serviceBusConnectionString = ConfigurationManager.AppSettings["serviceBus"];
+            var subscriptionClient = new SubscriptionClient(serviceBusConnectionString, topic, subscription);
+            subscriptionClient.RegisterMessageHandler(async (msg, cancelationToken) =>
+            {
+                var body = Encoding.UTF8.GetString(msg.Body);
+                Console.WriteLine(body);
+
+                await Task.CompletedTask;
+            },
+            async exception =>
+            {
+                await Task.CompletedTask;
+                // log exception
+            }
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
